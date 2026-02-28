84d04c1ac5ed9a8e6dedb3820863087eb82edc94
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-02-28 16:40:33 +0100

    Add files via upload

--- API_Gestion_Productos_Y_Pedidos/src/main/java/com/tiendaonline/gestion/model/Pedido.java
+++ API_Gestion_Productos_Y_Pedidos/src/main/java/com/tiendaonline/gestion/model/Pedido.java
@@ -1,1 +1,49 @@
+package com.tiendaonline.gestion.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+
+@Entity	// Anotación para indicar que esta clase es una entidad de JPA
+@Table(name = "pedidos")	// Anotación para especificar el nombre de la tabla en la BBDD
+public class Pedido {
+	
+	@Id 		// Anotación para indicar que este campo es la clave primaria de la entidad
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	// Anotación para indicar que el valor de este campo se generará automáticamente por la BBDD
+	private Long id;
+	
+	private LocalDateTime fecha;
+	private BigDecimal total;
+	
+	@Enumerated(EnumType.STRING)	// Anotación para indicar que el valor de este campo se almacenará como una cadena en la BBDD
+	private EstadoPedido estado;
+	
+	@OneToMany(mappedBy = "pedido", cascade =CascadeType.ALL, orphanRemoval = true)	// Anotación para indicar que esta entidad tiene una relación de uno a muchos con la entidad DeatellePedido
+	public List<DetallePedido> detalles = new ArrayList<>();
+	
+	@PrePersist	// Anotación para indicar que este método se ejecutará antes de que la entidad se persista en la BBDD
+	public void prePersist() {
+		this.fecha = LocalDateTime.now();
+		this.estado = EstadoPedido.CREADO;
+	}
+	
+    public void addDetalle(DetallePedido detalle) {
+        detalles.add(detalle);
+        detalle.setPedido(this);
+    }
+	
+	
+
+}
