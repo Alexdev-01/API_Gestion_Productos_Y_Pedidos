d0b37004f0c684abdd3579109884fb2a8be66a28
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-02-28 16:43:26 +0100

    Add files via upload

--- API_Gestion_Productos_Y_Pedidos/src/main/java/com/tiendaonline/repository/ProductoRepository.java
+++ API_Gestion_Productos_Y_Pedidos/src/main/java/com/tiendaonline/repository/ProductoRepository.java
@@ -1,1 +1,10 @@
+package com.tiendaonline.gestion.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import com.tiendaonline.gestion.model.Producto;
+
+public interface ProductoRepository  extends JpaRepository<Producto, Long>, JpaSpecificationExecutor<Producto> {
+
+}
