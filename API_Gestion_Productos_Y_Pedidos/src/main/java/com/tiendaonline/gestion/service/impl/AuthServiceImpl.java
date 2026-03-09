3c8d52c41467da719b94f1b63b7699aac42bc095
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-03-09 15:17:56 +0100

    Add files via upload

--- API_Gestion_Productos_Y_Pedidos/src/main/java/com/tiendaonline/gestion/service/impl/AuthServiceImpl.java
+++ API_Gestion_Productos_Y_Pedidos/src/main/java/com/tiendaonline/gestion/service/impl/AuthServiceImpl.java
@@ -1,7 +1,11 @@
 package com.tiendaonline.gestion.service.impl;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.tiendaonline.gestion.repository.UsuarioRepository;
+import com.tiendaonline.gestion.security.jwt.JwtService;
 import com.tiendaonline.gestion.service.AuthResponse;
 import com.tiendaonline.gestion.service.AuthService;
 import com.tiendaonline.gestion.service.LoginRequest;
@@ -9,6 +13,12 @@ import com.tiendaonline.gestion.service.RegisterRequest;
 
 @Service	// Marca esta clase como un servicio de Spring, lo que permite su gestión y uso en otras partes de la aplicación
 public class AuthServiceImpl implements AuthService {
+	
+	private final UsuarioRepository usuarioRepository;
+	private final PasswordEncoder passwordEncoder;
+	private final JwtService jwtService;
+	private final AuthenticationManager authenticationManager;
+	
 
 	@Override
 	public AuthResponse register(RegisterRequest request) {
