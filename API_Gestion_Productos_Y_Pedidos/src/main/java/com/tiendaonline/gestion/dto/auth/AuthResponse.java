3d17618b496cb039278ff062cf89d7e84c270d09
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-03-09 15:13:45 +0100

    Add files via upload

--- API_Gestion_Productos_Y_Pedidos/src/main/java/com/tiendaonline/gestion/dto/auth/AuthResponse.java
+++ API_Gestion_Productos_Y_Pedidos/src/main/java/com/tiendaonline/gestion/dto/auth/AuthResponse.java
@@ -1,1 +1,27 @@
+package com.tiendaonline.gestion.dto.auth;
 
+// Respuesta de autenticación que se devuelve después de un registro o login exitoso, generalmente contiene un token JWT.
+public class AuthResponse {
+	
+	private String token;
+
+	public AuthResponse() {
+		super();
+	}
+
+	public AuthResponse(String token) {
+		super();
+		this.token = token;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
+
+}
