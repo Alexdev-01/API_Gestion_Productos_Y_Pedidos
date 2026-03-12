b981f2e4279f5363b246c1dacb885bc3ca5bccd8
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-03-12 12:51:01 +0100

    Add files via upload

--- API_Gestion_Productos_Y_Pedidos/src/main/java/com/tiendaonline/gestion/service/impl/CategoriaServiceImpl.java
+++ API_Gestion_Productos_Y_Pedidos/src/main/java/com/tiendaonline/gestion/service/impl/CategoriaServiceImpl.java
@@ -2,11 +2,15 @@ package com.tiendaonline.gestion.service.impl;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.tiendaonline.gestion.model.Categoria;
 import com.tiendaonline.gestion.repository.CategoriaRepository;
+import com.tiendaonline.gestion.service.CategoriaService;
 
 @Service	// Marca esta clase como un componente de servicio en Spring
-public class CategoriaServiceImpl {
+// Implementa la interfaz CategoriaService para proporcionar la lógica de negocio relacionada con las categorías
+public class CategoriaServiceImpl implements CategoriaService {
 
 	private final CategoriaRepository categoriaRepository;
 
@@ -18,10 +22,33 @@ public class CategoriaServiceImpl {
 	public Categoria crearCategoria(Categoria categoria) {
 		return categoriaRepository.save(categoria);
 	}
+	
+	@Override
+	public Categoria obtenerPorId(Long id) {
+		return categoriaRepository.findById(id).orElseThrow(() -> new RuntimeException("Categoría no encontrada"));
+	}
+	
 
 	@Override
 	public List<Categoria> listarTodas() {
 		return categoriaRepository.findAll();
 	}
+	
 
+	@Override
+	public Categoria actualizarCategoria(Long id, Categoria categoria) {
+		
+		Categoria existente = obtenerPorId(id);
+		
+		existente.setNombre(categoria.getNombre());
+		existente.setDescripcion(categoria.getDescripcion());
+		
+		return categoriaRepository.save(existente);
+	}
+
+	@Override
+	public void eliminarCategoria(Long id) {
+		categoriaRepository.deleteById(id);
+	}
+	
 }
