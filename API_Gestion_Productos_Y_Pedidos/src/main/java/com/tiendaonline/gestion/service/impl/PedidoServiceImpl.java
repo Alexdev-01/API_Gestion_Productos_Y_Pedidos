31e0c268ad702336b8cfc51b3b8c5f556e21e274
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-03-14 11:37:12 +0100

    Add files via upload

--- API_Gestion_Productos_Y_Pedidos/src/main/java/com/tiendaonline/gestion/service/impl/PedidoServiceImpl.java
+++ API_Gestion_Productos_Y_Pedidos/src/main/java/com/tiendaonline/gestion/service/impl/PedidoServiceImpl.java
@@ -1,22 +1,35 @@
 package com.tiendaonline.gestion.service.impl;
 
+import java.math.BigDecimal;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.tiendaonline.gestion.dto.pedido.CrearPedidoRequest;
+import com.tiendaonline.gestion.dto.pedido.ItemPedidoRequest;
 import com.tiendaonline.gestion.model.Pedido;
+import com.tiendaonline.gestion.model.Producto;
 import com.tiendaonline.gestion.model.Usuario;
 import com.tiendaonline.gestion.repository.PedidoRepository;
+import com.tiendaonline.gestion.repository.ProductoRepository;
+import com.tiendaonline.gestion.repository.UsuarioRepository;
 import com.tiendaonline.gestion.service.PedidoService;
+
+import jakarta.transaction.Transactional;
 
 //Implementación de la interfaz PedidoService, que maneja la lógica de negocio relacionada con los pedidos
 @Service	// Marca esta clase como un componente de servicio en Spring
 public class PedidoServiceImpl implements PedidoService {
 	
 	private final PedidoRepository pedidoRepository;
+	private final ProductoRepository productoRepository;
+	private final UsuarioRepository usuarioRepository;
 	
-	public PedidoServiceImpl(PedidoRepository pedidoRepository) {
+	public PedidoServiceImpl(PedidoRepository pedidoRepository, UsuarioRepository usuarioRepository, ProductoRepository productoRepository) {
 		this.pedidoRepository = pedidoRepository;
+		this.productoRepository = productoRepository;
+		this.usuarioRepository = usuarioRepository;
 	}
 
 	
@@ -33,6 +46,29 @@ public class PedidoServiceImpl implements PedidoService {
 	@Override
 	public List<Pedido> obtenerTodos() {
 		return pedidoRepository.findAll();
+	}
+
+
+	@Override
+	@Transactional	// Asegura que todas las operaciones dentro de este método se ejecuten en una sola transacción
+	public Pedido crearPedido(CrearPedidoRequest request, String username) {
+		
+		Usuario usuario = usuarioRepository.findByUsername(username).orElseThrow();
+		
+		Pedido pedido = new Pedido();
+		pedido.setUsuario(usuario);
+		
+		BigDecimal total = BigDecimal.ZERO;
+		
+		for (ItemPedidoRequest item : request.getItems()) {
+			Producto producto = productoRepository.findById(item.getProductoId()).orElseThrow(() -> new RuntimeException("Producto no encontrado"));
+			
+			if (producto.getStock() < item.getCantidad()) {
+				throw new RuntimeException("Stock insuficiente para: " + producto.getNombre());
+			}
+		
+		
+		return null;
 	}
 	
 	
