83c701fe1f4f4f48c50af128d0a8ef1473a331de
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-03-16 15:15:34 +0100

    Add files via upload

--- API_Gestion_Productos_Y_Pedidos/src/main/java/com/tiendaonline/gestion/service/impl/PedidoServiceImpl.java
+++ API_Gestion_Productos_Y_Pedidos/src/main/java/com/tiendaonline/gestion/service/impl/PedidoServiceImpl.java
@@ -4,10 +4,12 @@ import java.math.BigDecimal;
 import java.util.Iterator;
 import java.util.List;
 
+import org.hibernate.cache.spi.support.AbstractReadWriteAccess.Item;
 import org.springframework.stereotype.Service;
 
 import com.tiendaonline.gestion.dto.pedido.CrearPedidoRequest;
 import com.tiendaonline.gestion.dto.pedido.ItemPedidoRequest;
+import com.tiendaonline.gestion.model.DetallePedido;
 import com.tiendaonline.gestion.model.Pedido;
 import com.tiendaonline.gestion.model.Producto;
 import com.tiendaonline.gestion.model.Usuario;
@@ -51,6 +53,7 @@ public class PedidoServiceImpl implements PedidoService {
 
 	@Override
 	@Transactional	// Asegura que todas las operaciones dentro de este método se ejecuten en una sola transacción
+	// Método para crear un nuevo pedido a partir de una solicitud y el nombre de usuario del cliente
 	public Pedido crearPedido(CrearPedidoRequest request, String username) {
 		
 		Usuario usuario = usuarioRepository.findByUsername(username).orElseThrow();
@@ -66,9 +69,27 @@ public class PedidoServiceImpl implements PedidoService {
 			if (producto.getStock() < item.getCantidad()) {
 				throw new RuntimeException("Stock insuficiente para: " + producto.getNombre());
 			}
+			
+			//Reducir Stock del producto
+			producto.setStock(producto.getStock() - Item.getCantidad());
+			
+			DetallePedido detalle = new DetallePedido();
+			
+			detalle.setProducto(producto);
+			detalle.setCantidad(item.getCantidad());
+			detalle.setPrecio(producto.getPrecio());
+			
+			detalle.setSubtotal(producto.getPrecio().multiply(BigDecimal.valueOf(item.getCantidad())));
+			
+			pedido.addDetalle(detalle);
+			
+			total = total.add(detalle.getSubtotal());
+			
+			}
 		
+		pedido.setTotal(total);
 		
-		return null;
+		return pedidoRepository.save(pedido);
 	}
 	
 	
